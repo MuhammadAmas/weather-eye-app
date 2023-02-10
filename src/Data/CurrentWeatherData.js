876027712623cc36d7b57e244b95a86b91b28504
muhammadamas <amaswaseem@gmail.com>
2023-02-10 21:39:31 +0500

    Updated

--- src/Data/CurrentWeatherData.js
+++ src/Data/CurrentWeatherData.js
@@ -2,4 +2,4 @@
 export const city = "Karachi";
 export const temprature = '33';
 export const day = 'Thurs';
-export const weather = 'sunny';
+export const weather = 'Sunny';
--- src/Data/DailyWeatherData.js
+++ src/Data/DailyWeatherData.js
@@ -2,4 +2,4 @@ export const temprature = [32, 33, 34, 35, 36, 37, 38]
 
 export const days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
 
-export const weather = ['sunny', 'rainy', 'cloudy', 'windy', 'sunny', 'sunny', 'rainy']
+export const weather = ['Sunny', 'Rainy', 'Cloudy', 'Windy', 'Sunny', 'Sunny', 'Rainy']
